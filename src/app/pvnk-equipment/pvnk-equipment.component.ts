f80b199301bb3c74d7ebf1cd7584fd2ea3dec601
Leo Velez <leonardo.velez@kaplan.edu>
2022-05-19 17:01:54 -0400

    Fix reroll app,cyber,nano assign with remove logic
    Add creds reroll logic

--- src/app/pvnk-equipment/pvnk-equipment.component.ts
+++ src/app/pvnk-equipment/pvnk-equipment.component.ts
@@ -57,80 +57,85 @@ export class PvnkEquipmentComponent implements OnInit, OnChanges {
   }
 
   reRollEquip(equip: any) {
-      // check if removing keywords nano, cybertech, Cyberdeck
-      const standardEquipKeys = Object.keys(this.equipArray).includes(equip);
-      const equipObj = standardEquipKeys ? this.equipArray : this.classSpecificEquipObj;
+      if (equip === 'creds') {
+        const creds = this.rollCreds();
+        this.equipment[0] = {key: 'creds', value: `<strong class="clickable">${creds}¤</strong> on an <strong>anonymous credstick</strong>`};
+      } else {
+        // check if removing keywords nano, cybertech, Cyberdeck
+        const standardEquipKeys = Object.keys(this.equipArray).includes(equip);
+        const equipObj = standardEquipKeys ? this.equipArray : this.classSpecificEquipObj;
 
-      for (const [key, value] of Object.entries(equipObj)) {
-        if (equip === key) {
-          let armorMod = 0;
+        for (const [key, value] of Object.entries(equipObj)) {
+            if (equip === key) {
+            let armorMod = 0;
 
-          let upperNum = this.equipMods[key] ? this.equipMods[key] : value.length;
-          if (key === 'armor' && !this.equipMods[key]) {
-              upperNum = 3;
-          } else if (key === 'armor' && this.equipMods[key] === 4) {
-              armorMod += 1;
-          }
-          
-          this.equipment.map(result => {
-              if (result.key === equip) {
-                  const prevEquipment = result.value;
+            let upperNum = this.equipMods[key] ? this.equipMods[key] : value.length;
+            if (key === 'armor' && !this.equipMods[key]) {
+                upperNum = 3;
+            } else if (key === 'armor' && this.equipMods[key] === 4) {
+                armorMod += 1;
+            }
+            
+            this.equipment.map(result => {
+                if (result.key === equip) {
+                    const prevEquipment = result.value;
 
-                  if (prevEquipment.includes('Cyberdeck') || prevEquipment.includes('cybertech') || prevEquipment.includes('nano')) {
-                    switch (true) {
-                        case prevEquipment.includes('Cyberdeck'): {
-                            if (this.equipMods.hasOwnProperty('nano')) {
-                                this.nano.splice(this.nano.length - 1);
-                            } else if (this.equipMods.hasOwnProperty('cybertech')) {
-                                this.cyberTech.splice(this.cyberTech.length - 1);
-                            } else {
-                                this.app.splice(this.app.length - 2);
+                    if (prevEquipment.includes('Cyberdeck') || prevEquipment.includes('cybertech') || prevEquipment.includes('nano')) {
+                        switch (true) {
+                            case prevEquipment.includes('Cyberdeck'): {
+                                if (this.equipMods.hasOwnProperty('nano')) {
+                                    this.nano.splice(this.nano.length - 1);
+                                } else if (this.equipMods.hasOwnProperty('cybertech')) {
+                                    this.cyberTech.splice(this.cyberTech.length - 1);
+                                } else {
+                                    this.app.splice(this.app.length - 2);
+                                }
+                                break;
                             }
-                            break;
-                        }
-                        case prevEquipment.includes('cybertech'): {
-                            if (this.equipMods.hasOwnProperty('nano')) {
-                                this.nano.splice(this.nano.length - 1);
-                            } else if (this.equipMods.hasOwnProperty('apps')) {
-                                this.app.splice(this.app.length - 1);
-                            } else {
-                                this.cyberTech.splice(this.cyberTech.length - 1);
+                            case prevEquipment.includes('cybertech'): {
+                                if (this.equipMods.hasOwnProperty('nano')) {
+                                    this.nano.splice(this.nano.length - 1);
+                                } else if (this.equipMods.hasOwnProperty('apps')) {
+                                    this.app.splice(this.app.length - 1);
+                                } else {
+                                    this.cyberTech.splice(this.cyberTech.length - 1);
+                                }
+                                break;
                             }
-                            break;
-                        }
-                        case prevEquipment.includes('nano'): {
-                            if (this.equipMods.hasOwnProperty('cybertech')) {
-                                this.cyberTech.splice(this.cyberTech.length - 1);
-                            } else if (this.equipMods.hasOwnProperty('apps')) {
-                                this.app.splice(this.app.length - 1);
-                            } else {
-                                this.nano.splice(this.nano.length - 1);
+                            case prevEquipment.includes('nano'): {
+                                if (this.equipMods.hasOwnProperty('cybertech')) {
+                                    this.cyberTech.splice(this.cyberTech.length - 1);
+                                } else if (this.equipMods.hasOwnProperty('apps')) {
+                                    this.app.splice(this.app.length - 1);
+                                } else {
+                                    this.nano.splice(this.nano.length - 1);
+                                }
+                                break;
                             }
-                            break;
                         }
                     }
-                  }
 
-                  const index = this.randomNumber.getRandomNumber(0, upperNum - 1, this.prevRollObj[key]);
-                  this.prevRollObj[key] = index;
-                  result.value = value[index  + armorMod];
-              }
-          });
+                    const index = this.randomNumber.getRandomNumber(0, upperNum - 1, this.prevRollObj[key]);
+                    this.prevRollObj[key] = index;
+                    result.value = value[index  + armorMod];
+                }
+            });
+            }
         }
-    }
-    for (const [key, value] of Object.entries(this.equipment)) {
-        if (key === equip) {
-            this.addAppsCyberNano(value);
+        for (const [key, value] of Object.entries(this.equipment)) {
+            // locate rolled section and mutate only that section
+            if (value.key === equip) {
+                this.addAppsCyberNano(value);
+            }
         }
-    }
-    this.emitData();
-
+        this.emitData();
+      }
   }
 
     public rollEquipment() {
-        const creds = this.randomNumber.rollMultipleDice(2, 6) * 10;
+        const creds = this.rollCreds();
         this.equipment = [
-            {key: 'default', value: `<strong class="clickable">${creds}¤</strong> on an <strong>anonymous credstick</strong>`},
+            {key: 'creds', value: `<strong class="clickable">${creds}¤</strong> on an <strong>anonymous credstick</strong>`},
             {key: 'default', value: `some <strong>cheap clothes</strong>`},
             {key: 'default', value: `a <strong>Retinal Com Device ( R C D )</strong>`}
         ];
@@ -141,6 +146,10 @@ export class PvnkEquipmentComponent implements OnInit, OnChanges {
 
         this.getEquipment();
         this.getClassSpecificEquipment();
+    }
+
+    private rollCreds() {
+        return this.randomNumber.rollMultipleDice(2, 6) * 10;
     }
 
     private getEquipment() {
